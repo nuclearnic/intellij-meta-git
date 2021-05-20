4f63a86072780715ae4ce1bda69f360089855e03
GitHub Action <action@github.com>
2021-05-20 21:28:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/nuclearnic/intellijmetagit/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nuclearnic/intellijmetagit/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nuclearnic.intellijmetagit.listeners
 
+import com.github.nuclearnic.intellijmetagit.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/nuclearnic/intellijmetagit/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nuclearnic.intellijmetagit.services
+
+import com.github.nuclearnic.intellijmetagit.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
